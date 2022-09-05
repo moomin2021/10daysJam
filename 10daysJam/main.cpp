26c535f74bff33ddc6b11438abdbf43090582791
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-09-05 11:17:41 +0900

    テスト

--- 10daysJam/main.cpp
+++ 10daysJam/main.cpp
@@ -44,7 +44,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	int iguiug8;
 	int fumiya;
 	int moomin;
-	int test0;
+	int test01;
 
 	int superrrrrrrrrrrrrrrr;
 
