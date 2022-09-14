ff8b1375eb1222d1329dbefc3d27096bacf23b4f
HirokiYamamoto <k021g1139@g.neec.ac.jp>
2022-09-14 10:10:41 +0900

    敵スポーン修正

--- 10daysJam/GameScene.cpp
+++ 10daysJam/GameScene.cpp
@@ -748,7 +748,7 @@ void GameScene::EnemySpawn(float radian) {
 	//Circle newPos = {enemyPos,8.0f}
 	newEnemy.SetObj({ {enemyPos} , 8.0f });
 
-	if (Random(0, 100) <= enemySpawnRate) {
+	if (Random(0, 100) < enemySpawnRate) {
 		//5%の確率で敵としてスポーン
 		newEnemy.SetState(State::Enemy);
 		newEnemy.SetHandle(enemyGraph);
